81c84ac0f780025e5492b611d7065cd014a2385d
xtyuns <xtyuns@163.com>
2023-07-23 19:52:28 +0800

    use const path
    ref: https://github.com/spring-cloud/spring-cloud-openfeign/issues/547

--- order-service/order-api/src/main/kotlin/org/example/order/client/OrderClient.kt
+++ order-service/order-api/src/main/kotlin/org/example/order/client/OrderClient.kt
@@ -3,8 +3,12 @@ package org.example.order.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.RequestMapping
 
-@FeignClient("order-service", path = "order")
+@FeignClient("order-service", path = OrderClient.PATH)
 interface OrderClient {
+    companion object {
+        const val PATH = "/order"
+    }
+
     @RequestMapping("/add")
     fun add(): String
 }
--- stock-service/stock-api/src/main/kotlin/org/example/stock/client/StockClient.kt
+++ stock-service/stock-api/src/main/kotlin/org/example/stock/client/StockClient.kt
@@ -3,8 +3,12 @@ package org.example.stock.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient("stock-service", path = "stock")
+@FeignClient("stock-service", path = StockClient.PATH)
 interface StockClient {
+    companion object {
+        const val PATH = "/stock"
+    }
+
     @GetMapping("/getName")
     fun getName(): String
 }
