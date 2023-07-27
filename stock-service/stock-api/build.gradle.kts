6aec7d3d2f8341438eff7df39aabaf91c5ef6203
xtyuns <xtyuns@163.com>
2023-07-27 14:10:02 +0800

    move dependencies

--- stock-service/stock-api/build.gradle.kts
+++ stock-service/stock-api/build.gradle.kts
@@ -8,6 +8,4 @@ version = "unspecified"
 dependencies {
     implementation(project(":common-library"))
     api("org.springframework.cloud:spring-cloud-starter-openfeign")
-    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
 }
